d0706488940505f6a8a3687381ffad8beca11408
ShapovalovArtur <shapovalov669@gmail.com>
2020-07-29 15:42:19 +0300

    Arr1

---
+++ js/data.js
@@ -0,0 +1,44 @@
+var smallArr = [
+    {
+        id: 101,
+        firstName: 'Sue',
+        lastName: 'Corson',
+        email: 'DWhalley@in.gov',
+        phone: '(612)211-6296',
+        address: {
+            streetAddress: '9792 Mattis Ct',
+            city: 'Waukesha',
+            state: 'WI',
+            zip: '22178'
+        },
+        description: 'et lacus magna dolor...'
+    },
+    {
+        id: 102,
+        firstName: 'Hue',
+        lastName: 'Gordon',
+        email: 'DWhy@in.ov',
+        phone: '(61-6296',
+        address: {
+            streetAddress: '9792 Mats Ct',
+            city: 'Fsha',
+            state: 'WEI',
+            zip: '22378'
+        },
+        description: 'wet wlac'
+    },
+    {
+        id: 103,
+        firstName: 'Drue',
+        lastName: 'Werson',
+        email: 'wwehalley@in.gov',
+        phone: '(612)321-626',
+        address: {
+            streetAddress: '22 attis Ct',
+            city: 'Vreesha',
+            state: 'WI',
+            zip: '321178'
+        },
+        description: 'Xers mrr...'
+    }
+]
